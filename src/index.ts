b485b4a68662e8cad182907372c7178530bf01a6
Tomás Lopez Hidalgo <tflopezhidalgo@gmail.com>
2022-10-05 20:52:24 -0300

    Attach to PORT env var

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello world!');
+app.get('/', (_, res) => {
+  res.send('Welcome to TeachHub!');
 });
 
 app.listen(port, () => {
-  console.log("Starting app....");
+  console.log(`Listening on ${port}`);
 })
