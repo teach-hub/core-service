14311206ef32718857b69d0685cf533f6ef8a874
Tomás López Hidalgo <tflopezhidalgo@gmail.com>
2022-10-24 00:38:49 -0300

    Usar puerto 4000 en vez de 3000 por defecto (#5)

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import { graphqlHTTP } from 'express-graphql';
 import { writeSchema } from './utils';
 import schema from './schema';
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 const app = express();
 
