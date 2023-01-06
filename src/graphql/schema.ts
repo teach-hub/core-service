46aec28a952f1d8f0728ddf455df2174fe7121fb
Tomás Lopez Hidalgo <tflopezhidalgo@gmail.com>
2023-01-06 01:01:37 -0300

    Start populating main schema

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,16 +1,48 @@
-import { GraphQLSchema, GraphQLObjectType } from 'graphql';
+import {
+  GraphQLID,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLObjectType,
+} from "graphql";
+
+const UserType = new GraphQLObjectType({
+  name: "UserType",
+  fields: {
+    name: {
+      type: GraphQLString,
+    },
+    surname: {
+      type: GraphQLString,
+    },
+  },
+});
+
+const Query = new GraphQLObjectType({
+  name: "RootQueryType",
+  description: "Root query",
+  fields: {
+    viewer: {
+      description: "Logged in user",
+      type: UserType,
+      resolve: () => ({ name: null, surname: null }),
+    },
+    findUser: {
+      type: UserType,
+      args: { id: { type: GraphQLID } },
+      resolve: () => ({ name: null, surname: null }),
+    },
+  },
+});
+
+const Mutation = new GraphQLObjectType({
+  name: "RootMutationType",
+  description: "Root mutation",
+  fields: {},
+});
 
 const schema = new GraphQLSchema({
-  query: new GraphQLObjectType({
-    name: 'RootQueryType',
-    description: 'Root query',
-    fields: {},
-  }),
-  mutation: new GraphQLObjectType({
-    name: 'RootMutationType',
-    description: 'Root mutation',
-    fields: {},
-  }),
+  query: Query,
+  // mutation: Mutation
 });
 
 export default schema;
