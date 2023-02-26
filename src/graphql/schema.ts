c316e7e6cea52774b601ed27cd2dc3bb8519d662
Tomás Lopez Hidalgo <tflopezhidalgo@gmail.com>
2023-02-26 16:29:20 -0300

    Add subjects field to viewer

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,7 +1,14 @@
-import { GraphQLSchema, GraphQLObjectType } from 'graphql';
+import {
+  GraphQLNonNull,
+  GraphQLSchema,
+  GraphQLObjectType,
+  Source,
+  GraphQLList,
+} from 'graphql';
 
 import { userMutations, userFields, UserType } from '../lib/user/internalGraphql';
 import { findAllUsers } from '../lib/user/userService';
+import { SubjectType } from '../lib/subject/graphql';
 
 import type { Context } from 'src/types';
 
@@ -26,13 +33,31 @@ const getViewer = async (ctx: Context) => {
   };
 };
 
+const ViewerType = new GraphQLObjectType({
+  name: 'ViewerType',
+  fields: {
+    userInfo: {
+      description: 'User related information from viewer',
+      type: new GraphQLNonNull(UserType),
+      resolve: getViewer,
+    },
+
+    // TODO(Tomas): esto tiene que ser una connection.
+    subjects: {
+      description: 'Subjects belonging to viewer',
+      type: new GraphQLNonNull(new GraphQLList(SubjectType)),
+      resolve: () => [],
+    },
+  },
+});
+
 const Query: GraphQLObjectType<null, Context> = new GraphQLObjectType({
   name: 'RootQueryType',
   description: 'Root query',
   fields: {
     viewer: {
       description: 'Logged in user',
-      type: UserType,
+      type: ViewerType,
       resolve: async (_source, _args, ctx) => getViewer(ctx),
     },
     ...userFields,
