2a8eead7ad95acb690700fde813e29e7b5b018b3
spenna <sebapenna19@gmail.com>
2023-03-31 20:29:23 -0300

    TH-57 Add logout mutation

--- src/github/auth.ts
+++ src/github/auth.ts
@@ -1,4 +1,6 @@
 import { githubOAuthApp } from './config';
+import logger from '../logger';
+import * as OAuthMethods from '@octokit/oauth-methods';
 
 export const exchangeCodeForToken = async (code: string): Promise<string> => {
   const { authentication } = await githubOAuthApp.createToken({
@@ -6,4 +8,20 @@ export const exchangeCodeForToken = async (code: string): Promise<string> => {
   });
 
   return authentication.token;
+};
+
+/**
+ * Returns true if the token was revoked,
+ * false otherwise.
+ * */
+export const revokeToken = async (token: string): Promise<boolean> => {
+  try {
+    const response: OAuthMethods.DeleteTokenResponse = await githubOAuthApp.deleteToken({
+      token: token,
+    });
+    if (response.status === 204) return true;
+  } catch (error) {
+    logger.error(error);
+  }
+  return false;
 };
