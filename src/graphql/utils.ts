34b713c6d6b1cd0079c38a693ec24e1665ceebfa
spenna <sebapenna19@gmail.com>
2023-03-31 18:54:11 -0300

    Replace deprecated btoa and atob functions

--- src/graphql/utils.ts
+++ src/graphql/utils.ts
@@ -11,11 +11,11 @@ type GlobalId = string;
 type EntityPayload = { entityName: string; dbId: string };
 
 export const toGlobalId = ({ dbId, entityName }: EntityPayload): GlobalId => {
-  return btoa(`${entityName}:${dbId}`);
+  return Buffer.from(`${entityName}:${dbId}`).toString('base64');
 };
 
 export const fromGlobalId = (globalId: GlobalId): EntityPayload => {
-  const decoded = atob(globalId).split(':');
+  const decoded = Buffer.from(globalId, 'base64').toString().split(':');
   return { entityName: decoded[0], dbId: decoded[1] };
 };
 
