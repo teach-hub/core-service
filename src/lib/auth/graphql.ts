118b0fddf656f1e8c347e7fbae0d1573dca7f9b9
Sebastian Penna <31221128+sebapenna@users.noreply.github.com>
2023-04-11 00:44:25 -0300

    TH-57 Add logout mutation (#36)

--- src/lib/auth/graphql.ts
+++ src/lib/auth/graphql.ts
@@ -6,11 +6,19 @@ import {
   GraphQLFieldConfigMap,
 } from 'graphql';
 import type { Context } from '../../types';
-import { exchangeCodeForToken } from '../../github/auth';
+import { exchangeCodeForToken, revokeToken } from '../../github/auth';
 
 export const Login: GraphQLObjectType<unknown, Context> = new GraphQLObjectType({
   name: 'Login',
   description: 'Authenticated data',
+  fields: {
+    token: { type: GraphQLString },
+  },
+});
+
+export const Logout: GraphQLObjectType<unknown, Context> = new GraphQLObjectType({
+  name: 'Logout',
+  description: 'Logout data',
   fields: {
     token: { type: GraphQLString },
   },
@@ -28,6 +36,26 @@ export const authMutations: GraphQLFieldConfigMap<unknown, Context> = {
 
       return {
         token: await exchangeCodeForToken(code),
+      };
+    },
+  },
+  logout: {
+    type: Logout,
+    description: 'Logout user',
+    args: {
+      token: { type: GraphQLString },
+    },
+    resolve: async (_, args, ctx: Context) => {
+      const { token } = args;
+
+      const tokenRevoked: boolean = await revokeToken(token);
+
+      tokenRevoked
+        ? ctx.logger.info(`Revoked token ${token}`)
+        : ctx.logger.info(`Failed to revoke token ${token}`);
+
+      return {
+        token: null, // Return null field as graphql does not allow empty objects
       };
     },
   },
