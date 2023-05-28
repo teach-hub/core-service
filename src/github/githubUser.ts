e7ff3ca0b4c9bbfe43bfc0d7150ba5c0cf7240ff
Sebastian Penna <31221128+sebapenna@users.noreply.github.com>
2023-05-27 23:28:07 -0300

    TH-115 Configure github organization (#48)

--- src/github/githubUser.ts
+++ src/github/githubUser.ts
@@ -2,9 +2,7 @@ import { Octokit } from '@octokit/rest';
 import { getGithubToken } from '../tokens/jwt';
 
 export const getGithubUserIdFromGithubToken = async (token: string): Promise<string> => {
-  const octokit = new Octokit({
-    auth: token,
-  });
+  const octokit = initOctokit(token);
 
   const {
     data: { id },
@@ -12,7 +10,22 @@ export const getGithubUserIdFromGithubToken = async (token: string): Promise<str
 
   return String(id);
 };
+
 export const getGithubUserId = async (jwt: string): Promise<string> => {
   const token = getGithubToken({ token: jwt });
   return getGithubUserIdFromGithubToken(token);
+};
+
+export const getGithubUserOrganizationNames = async (jwt: string): Promise<string[]> => {
+  const token = getGithubToken({ token: jwt });
+  const octokit = initOctokit(token);
+
+  const response = await octokit.rest.orgs.listForAuthenticatedUser();
+  return response.data.map(org => org.login);
+};
+
+const initOctokit = (token: string) => {
+  return new Octokit({
+    auth: token,
+  });
 };
