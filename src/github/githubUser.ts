c81e3e2b2117c56f85e54618ff3dbf9eceb229bb
Sebastian Penna <31221128+sebapenna@users.noreply.github.com>
2023-06-20 12:22:03 -0300

    TH-117 Create individual repositories (#54)

--- src/github/githubUser.ts
+++ src/github/githubUser.ts
@@ -1,8 +1,9 @@
-import { Octokit } from '@octokit/rest';
 import { getGithubToken } from '../tokens/jwt';
+import { initOctokit, initOctokitWithGithubToken } from './config';
+import { Octokit } from '@octokit/rest';
 
 export const getGithubUserIdFromGithubToken = async (token: string): Promise<string> => {
-  const octokit = initOctokit(token);
+  const octokit = initOctokitWithGithubToken(token);
 
   const {
     data: { id },
@@ -17,15 +18,22 @@ export const getGithubUserId = async (jwt: string): Promise<string> => {
 };
 
 export const getGithubUserOrganizationNames = async (jwt: string): Promise<string[]> => {
-  const token = getGithubToken({ token: jwt });
-  const octokit = initOctokit(token);
+  const octokit = initOctokit(jwt);
 
   const response = await octokit.rest.orgs.listForAuthenticatedUser();
   return response.data.map(org => org.login);
 };
 
-const initOctokit = (token: string) => {
-  return new Octokit({
-    auth: token,
+/**
+ * Returns the GitHub username from a GitHub id
+ * */
+export const getGithubUsernameFromGithubId = async (
+  octokit: Octokit,
+  githubId: string
+): Promise<string> => {
+  /* Octokit does not have a method linked to this request */
+  const response = await octokit.request('GET /user/:id', {
+    id: githubId,
   });
+  return response.data.login;
 };
