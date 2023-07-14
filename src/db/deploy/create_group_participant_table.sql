70de827e7ac3c149389ecf1d8f87d83ebb6268af
spenna <sebapenna19@gmail.com>
2023-07-13 21:12:32 -0300

    Th-148 Fix create group participant table migration

--- src/db/deploy/create_group_participant_table.sql
+++ src/db/deploy/create_group_participant_table.sql
@@ -7,7 +7,6 @@ CREATE TABLE teachhub.group_participants (
     assignment_id             INTEGER REFERENCES teachhub.assignments(id) NOT NULL,
     group_id             INTEGER REFERENCES teachhub.groups(id) NOT NULL,
     user_role_id             INTEGER REFERENCES teachhub.user_roles(id) NOT NULL,
-    name                  TEXT NOT NULL,
     active                  BOOLEAN NOT NULL
   );
 
