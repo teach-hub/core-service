c733769af5d3b803006173ae6376298e4376739e
Tomás Lopez Hidalgo <tflopezhidalgo@gmail.com>
2023-07-23 01:32:47 -0300

    Add uniqueness

--- src/db/deploy/add_group_id_reviewers.sql
+++ src/db/deploy/add_group_id_reviewers.sql
@@ -4,7 +4,10 @@ BEGIN;
 
 ALTER TABLE teachhub.reviewers
 DROP COLUMN reviewee_user_id,
-ADD COLUMN reviewee_id INTEGER NOT NULL;
+ADD COLUMN reviewee_id INTEGER NOT NULL,
+
+-- No podemos tener un reviewee con dos reviewers.
+ADD CONSTRAINT unique_assigment_reviewee UNIQUE (assignment_id, reviewee_id);
 
 CREATE INDEX assignment_idx ON teachhub.reviewers(assignment_id);
 
--- src/db/revert/add_group_id_reviewers.sql
+++ src/db/revert/add_group_id_reviewers.sql
@@ -3,8 +3,12 @@
 BEGIN;
 
 ALTER TABLE teachhub.reviewers
+
+-- Droppear la columna tambien dropea
+-- las constraints asociadas.
 DROP COLUMN reviewee_id,
 ADD COLUMN reviewee_user_id INTEGER REFERENCES teachhub.users(id) NOT NULL;
+
 
 DROP INDEX teachhub.assignment_idx;
 
