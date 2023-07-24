a2f4c8092b8877a44c86f3407333d4f43c54bd99
Tomás Lopez Hidalgo <tflopezhidalgo@gmail.com>
2023-07-23 21:03:14 -0300

    Add trigger to emulate foreign keys

--- src/db/deploy/add_reviewee_id_reviewers.sql
+++ src/db/deploy/add_reviewee_id_reviewers.sql
@@ -10,4 +10,17 @@ ADD COLUMN reviewee_id INTEGER NOT NULL,
 -- No podemos tener un reviewee con dos reviewers.
 ADD CONSTRAINT unique_assigment_reviewee UNIQUE (assignment_id, reviewee_id);
 
+CREATE FUNCTION check_reviewee_is_user_or_group() RETURNS trigger AS $check_is_group_or_user$
+    BEGIN
+        IF NEW.reviewee_id NOT IN (SELECT id FROM teachhub.users UNION SELECT id FROM teachhub.groups) THEN
+            RAISE EXCEPTION 'reviewe_id must belong to groups or users';
+        END IF;
+        RETURN NEW;
+    END;
+$check_is_group_or_user$ LANGUAGE plpgsql;
+
+CREATE TRIGGER check_is_group_or_user
+BEFORE INSERT OR UPDATE ON teachhub.reviewers
+FOR EACH ROW EXECUTE PROCEDURE check_reviewee_is_user_or_group();
+
 COMMIT;
--- src/db/revert/add_reviewee_id_reviewers.sql
+++ src/db/revert/add_reviewee_id_reviewers.sql
@@ -10,4 +10,7 @@ DROP COLUMN reviewee_id,
 ADD COLUMN reviewee_user_id INTEGER REFERENCES teachhub.users(id),
 ADD COLUMN reviewee_group_id INTEGER REFERENCES teachhub.groups(id);
 
+DROP TRIGGER IF EXISTS check_is_group_or_user ON teachhub.reviewers;
+DROP FUNCTION IF EXISTS check_reviewee_is_user_or_group;
+
 COMMIT;
